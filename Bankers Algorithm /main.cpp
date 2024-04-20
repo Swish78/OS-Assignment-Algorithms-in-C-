af79dc9b7b81e54c3c2aa86e513d11a54459a95f
Swayam Patil <98048555+Swish78@users.noreply.github.com>
2024-04-20 23:09:33 +0530

    Update main.cpp

--- Bankers Algorithm /main.cpp
+++ Bankers Algorithm /main.cpp
@@ -1,119 +1,63 @@
+/*
+Criteria = Resource Allocation
+Mode = Banker's Algorithm (Safety Algorithm)
+*/
+
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-// Number of processes and resources
-int num_processes, num_resources;
-
-// Available resources
-vector<int> available;
-
-// Maximum resources needed by each process
-vector<vector<int>> max_need;
-
-// Resources currently allocated to each process
-vector<vector<int>> allocation;
-
-// Resources still needed by each process
-vector<vector<int>> need;
-
-// Safety sequence
-vector<int> safe_sequence;
-
-// Function to check if the system is in a safe state
-bool is_safe() {
-    vector<bool> finished(num_processes, false);
-    int count = 0;
-
-    while (count < num_processes) {
-        bool found = false;
-        for (int i = 0; i < num_processes; i++) {
-            if (!finished[i]) {
-                bool can_allocate = true;
-                for (int j = 0; j < num_resources; j++) {
-                    if (need[i][j] > available[j]) {
-                        can_allocate = false;
-                        break;
-                    }
-                }
-                if (can_allocate) {
-                    for (int j = 0; j < num_resources; j++) {
-                        available[j] += allocation[i][j];
-                    }
-                    finished[i] = true;
-                    safe_sequence.push_back(i);
-                    count++;
-                    found = true;
-                    break;
-                }
-            }
-        }
-        if (!found) {
-            return false;
-        }
-    }
-
-    return true;
-}
+#define el "\n"
+#define td "\t"
 
 int main() {
-    cout << "Banker's Algorithm\n ----------------------------------------------------------\n";
+    int maxR[3] = {10, 5, 7};  // Maximum available resources
+    int alloc[5][3] = {{0, 1, 0}, {2, 0, 0}, {3, 0, 2}, {2, 1, 1}, {0, 0, 2}};  // Allocated resources
+    int max[5][3] = {{7, 5, 3}, {3, 2, 2}, {9, 0, 2}, {2, 2, 2}, {4, 3, 3}};  // Maximum required resources
 
-    // Get the number of processes and resources
-    cout << "Enter the number of processes: ";
-    cin >> num_processes;
-    cout << "Enter the number of resources: ";
-    cin >> num_resources;
+    int need[5][3];  // Need matrix
+    int available[3] = {3, 3, 2};  // Initially available resources
 
-    // Get the available resources
-    cout << "Enter the available resources (separated by spaces): ";
-    available.resize(num_resources);
-    for (int i = 0; i < num_resources; i++) {
-        cin >> available[i];
-    }
-
-    // Get the maximum resources needed by each process
-    max_need.resize(num_processes, vector<int>(num_resources, 0));
-    cout << "Enter the maximum resources needed by each process:\n";
-    for (int i = 0; i < num_processes; i++) {
-        cout << "Process " << i << ": ";
-        for (int j = 0; j < num_resources; j++) {
-            cin >> max_need[i][j];
+    // Calculate need matrix
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            need[i][j] = max[i][j] - alloc[i][j];
         }
     }
 
-    // Get the resources currently allocated to each process
-    allocation.resize(num_processes, vector<int>(num_resources, 0));
-    cout << "Enter the resources currently allocated to each process:\n";
-    for (int i = 0; i < num_processes; i++) {
-        cout << "Process " << i << ": ";
-        for (int j = 0; j < num_resources; j++) {
-            cin >> allocation[i][j];
+    int vis[5] = {-1, -1, -1, -1, -1};  // Track visited processes
+    int seq[5] = {-1, -1, -1, -1, -1};  // Store the safe sequence
+    int index = 0;
+
+    // Banker's Algorithm
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (vis[j] == -1 && (need[j][0] <= available[0]) && (need[j][1] <= available[1]) && (need[j][2] <= available[2])) {
+                vis[j] = j;
+                seq[index++] = j;
+                for (int k = 0; k < 3; k++) {
+                    available[k] += alloc[j][k];
+                }
+                break;
+            }
         }
     }
 
-    // Calculate the resources still needed by each process
-    need.resize(num_processes, vector<int>(num_resources, 0));
-    for (int i = 0; i < num_processes; i++) {
-        for (int j = 0; j < num_resources; j++) {
-            need[i][j] = max_need[i][j] - allocation[i][j];
+    int i = 0;
+    for (i = 0; i < 5; i++) {
+        if (seq[i] == -1) {
+            cout << "Sequence is invalid" << el;
+            break;
         }
     }
 
-    // Check if the system is in a safe state
-    if (is_safe()) {
-        cout << "The system is in a safe state.\n";
-        cout << "Safe sequence: ";
-        for (int i = 0; i < safe_sequence.size(); i++) {
-            cout << "P" << safe_sequence[i] << " ";
+    if (i == 5) {
+        cout << "Sequence is: ";
+        for (int i = 0; i < 5; i++) {
+            cout << "P" << seq[i] << " ";
         }
-        cout << "\n";
-    } else {
-        cout << "The system is not in a safe state.\n";
+        cout << el;
     }
 
     return 0;
 }
-
