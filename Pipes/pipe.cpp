149374a63acfdc86e9aa23bc9ff1853c83139713
Swayam Patil <98048555+Swish78@users.noreply.github.com>
2024-04-20 23:19:40 +0530

    Update pipe.cpp

--- Pipes/pipe.cpp
+++ Pipes/pipe.cpp
@@ -1,55 +1,150 @@
 #include <iostream>
 #include <unistd.h>
-#include <sys/types.h>
+#include <cstring>
 #include <sys/wait.h>
-#include <string.h>
 
 using namespace std;
 
 #define el "\n"
 #define td "\t"
+#define line "----------------------"
+
+int twoWayCommunication() {
+    int fromChild[2], toChild[2];
+	
+    if (pipe(fromChild) == -1 || pipe(toChild) == -1) {
+        cout << "pipe failed" << el;
+        return 1;
+    }
+	
+    int id = fork();
+	
+    if (id == -1) {
+        cout << "fork failed" << el;
+        return 2;
+    }
+	
+    if (id == 0) {
+        // Child process
+        close(toChild[1]);
+        close(fromChild[0]);
+		
+        char s1[100], s2[100];
+		
+        if (read(toChild[0], &s1, sizeof(s1)) == -1) {
+            cout << "read failed" << el;
+            return 3;
+        }
+		
+        cout << "(CHILD)---> received from parent: " << s1 << el;
+        cout << "(CHILD)----> enter the string to be concatenated with the string got from parent: ";
+        cin >> s2;
+		
+        strcat(s1, " ");
+        strcat(s1, s2);
+		
+        if (write(fromChild[1], &s1, sizeof(s1)) == -1) {
+            cout << "write failed" << el;
+            return 4;
+        }
+		
+        close(toChild[0]);
+        close(fromChild[1]);
+    } else {
+        close(toChild[0]);
+        close(fromChild[1]);
+		
+        char s1[100];
+		
+        cout << "(PARENT) --> enter the string: ";
+        cin >> s1;
+		
+        if (write(toChild[1], &s1, sizeof(s1)) == -1) {
+            cout << "write failed" << el;
+            return 5;
+        }
+		
+        if (read(fromChild[0], &s1, sizeof(s1)) == -1) {
+            cout << "read failed" << el;
+            return 6;
+        }
+		
+        cout << "(PARENT)---> received from child: " << s1 << el;
+		
+        close(toChild[1]);
+        close(fromChild[0]);
+    }
+	
+    return 2;
+}
+
+int oneWayCommunication() {
+    int fd[2];
+	
+    if (pipe(fd) == -1) {
+        cout << "pipe failed" << el;
+        return 1;
+    }
+	
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int start, end, sum = 0;
+	
+    int id = fork();
+	
+    if (id == -1) {
+        cout << "fork failed" << el;
+        return 2;
+    }
+	
+    start = (id == 0) ? 0 : n / 2;
+    end = (id == 0) ? n / 2 : n;
+	
+    for (int i = start; i < end; i++) {
+        sum += arr[i];
+    }
+	
+    if (id == 0) {
+        close(fd[0]);
+		
+        if (write(fd[1], &sum, sizeof(sum)) == -1) {
+            cout << "Write failed" << el;
+            return 3;
+        }
+		
+        close(fd[1]);
+    } else {
+        int pSum;
+        close(fd[1]);
+		
+        if (read(fd[0], &pSum, sizeof(pSum)) == -1) {
+            cout << "read failed" << el;
+            return 3;
+        }
+		
+        cout << "Sum of complete array is: " << (sum + pSum) << el;
+        close(fd[0]);
+    }
+	
+    return 5;
+}
 
 int main() {
-    int fd[2];
-    pid_t pid;
-    char buffer[1024];
-
-    cout << "----------------------------------------------" << el;
-    cout << "Inter-Process Communication using Pipes" << el;
-    cout << "----------------------------------------------" << el;
-
-    if (pipe(fd) == -1) {
-        cout << "Error creating pipe" << el;
-        return 1;
+    int choice;
+	
+    cout << "Enter 1 for one way communication:" << el;
+    cout << "Enter 2 for two way communication:" << el;
+    cin >> choice;
+	
+    cout << line << el;
+	
+    if (choice == 1) {
+        oneWayCommunication();
+    } else if (choice == 2) {
+        twoWayCommunication();
     }
-
-    pid = fork();
-
-    if (pid == -1) {
-        cout << "Error creating child process" << el;
-        return 1;
-    } else if (pid == 0) { // Child process
-        close(fd[1]); // Close the write end of the pipe
-
-        int bytes_read = read(fd[0], buffer, 1024);
-        if (bytes_read == -1) {
-            cout << "Error reading from pipe" << el;
-            return 1;
-        }
-
-        cout << "Child process received message: " << buffer << el;
-        close(fd[0]); // Close the read end of the pipe
-    } else { // Parent process
-        close(fd[0]); // Close the read end of the pipe
-
-        const char* message = "Hello from parent process!";
-        write(fd[1], message, strlen(message) + 1);
-        close(fd[1]); // Close the write end of the pipe
-
-        int status;
-        waitpid(pid, &status, 0); // Wait for the child process to finish
-    }
-
-    cout << "----------------------------------------------" << el;
+	
+    cout << line << el;
+	
     return 0;
 }
