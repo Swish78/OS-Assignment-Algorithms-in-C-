5de1712ea7b32c654a830c83d7fce72f85d4f879
Swayam Patil <98048555+Swish78@users.noreply.github.com>
2024-04-22 08:26:34 +0530

    Update main.cpp

--- Bankers Algorithm /main.cpp
+++ Bankers Algorithm /main.cpp
@@ -3,61 +3,48 @@ Criteria = Resource Allocation
 Mode = Banker's Algorithm (Safety Algorithm)
 */
 
-#include <iostream>
-
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-#define el "\n"
-#define td "\t"
-
 int main() {
-    int maxR[3] = {10, 5, 7};  // Maximum available resources
-    int alloc[5][3] = {{0, 1, 0}, {2, 0, 0}, {3, 0, 2}, {2, 1, 1}, {0, 0, 2}};  // Allocated resources
-    int max[5][3] = {{7, 5, 3}, {3, 2, 2}, {9, 0, 2}, {2, 2, 2}, {4, 3, 3}};  // Maximum required resources
-
-    int need[5][3];  // Need matrix
-    int available[3] = {3, 3, 2};  // Initially available resources
-
-    // Calculate need matrix
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 3; j++) {
-            need[i][j] = max[i][j] - alloc[i][j];
+    vector<int> MaxR = {10, 15, 20};
+    vector< vector<int>> AllocR = {{0, 1, 0}, {2, 0, 0}, {3, 0, 2}, {2, 1, 1}, {0, 0, 2}};
+    vector< vector<int>> Max = {{7, 5, 3}, {3, 2, 2}, {9, 0, 2}, {2, 2, 2}, {4, 3, 3}};
+    vector<int> Available = {5, 10 , 15};
+    vector< vector<int>> need(5, vector<int>(3));
+    vector<int> visited(5,-1);
+    vector<int> sequence(5,-1);
+    
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j< 3 ; j ++){
+            need[i][j] = (Max[i][j] - AllocR[i][j]);
         }
     }
-
-    int vis[5] = {-1, -1, -1, -1, -1};  // Track visited processes
-    int seq[5] = {-1, -1, -1, -1, -1};  // Store the safe sequence
     int index = 0;
-
-    // Banker's Algorithm
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            if (vis[j] == -1 && (need[j][0] <= available[0]) && (need[j][1] <= available[1]) && (need[j][2] <= available[2])) {
-                vis[j] = j;
-                seq[index++] = j;
-                for (int k = 0; k < 3; k++) {
-                    available[k] += alloc[j][k];
+    
+    for(int i = 0; i< 5; i++){
+        for(int j = 0; j < 5;j++){
+            if(visited[j] == -1 && need[j][0] <= Available[0] && need[j][1] <= Available[1] && need[j][2] <= Available[2]){
+                visited[j] = j;
+                sequence[index++] = j;
+                for(int k = 0 ;k < 3; k++){
+                    Available[k] += AllocR[j][k];
                 }
                 break;
             }
         }
     }
-
-    int i = 0;
-    for (i = 0; i < 5; i++) {
-        if (seq[i] == -1) {
-            cout << "Sequence is invalid" << el;
-            break;
+    
+    if(find(sequence.begin(), sequence.end(), -1) != sequence.end()){
+        cout << "deadlock"<<endl;
+    }
+    else{
+        for(auto i : sequence){
+            cout << i << "\t";
         }
     }
-
-    if (i == 5) {
-        cout << "Sequence is: ";
-        for (int i = 0; i < 5; i++) {
-            cout << "P" << seq[i] << " ";
-        }
-        cout << el;
-    }
-
     return 0;
 }
+
